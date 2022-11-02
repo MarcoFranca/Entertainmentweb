04f62991d7bad070cb5c4a61552ef991f27fd06a
Marco Franca <dev.marcotullio@gmail.com>
2022-11-02 18:27:41 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+
+      Search for movies or TV series
+
+      Trending
+
+      <!-- Display trending shows -->
+
+      Recommended for you
+
+      <!-- Display recommended shows -->
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
