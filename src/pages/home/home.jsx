6180c9f56646867283d9bb4264b081c3cb77642c
Marco Franca <dev.marcotullio@gmail.com>
2022-11-03 19:18:20 -0300

    added home and page file not found [add]

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <main>
+
+        Search for movies or TV series
+
+        Trending
+
+        {/*// <!-- Display trending shows -->*/}
+
+        Recommended for you
+
+        {/*// <!-- Display recommended shows -->*/}
+
+        </main>
+    )
+}
