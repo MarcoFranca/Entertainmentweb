888e305fdc34c1f634163ffd3d412ffe86831062
Marco Franca <dev.marcotullio@gmail.com>
2022-11-06 13:26:49 -0300

    inclusion of home and singnup pages [update]

--- src/assets/router/router.jsx
+++ src/assets/router/router.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "../../pages/home/home";
 import PageNotFound from "../../pages/error/pageNotFound";
+import Login from "../../pages/login/login";
+import Singnup from "../../pages/singnup/singnup";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<Home/>}/>
+                <Route index element={<Login/>}/>
+                <Route path="/singnup" element={<Singnup/>}/>
+                <Route path="/home" element={<Home/>}/>
                 <Route path="*" element={<PageNotFound/>}/>
             </Routes>
         </BrowserRouter>
