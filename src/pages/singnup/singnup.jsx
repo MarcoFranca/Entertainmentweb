220cf34158187df3abf550009b1813a81df36207
Marco Franca <dev.marcotullio@gmail.com>
2022-11-08 14:47:32 -0300

    singnup page styling [UPDATE]

--- src/pages/singnup/singnup.jsx
+++ src/pages/singnup/singnup.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {ButtonRed, LinkStyle, MainSingnup, MainSingnupForm, MainSingnupFormContent} from "./singnupStyle";
+import movieLogo from "../../assets/images/logo.svg"
 
 export default function Singnup() {
     return (
-        <form>
-            <img src="" alt=""/>
-            <h1>Sign Up</h1>
+        <MainSingnup>
+            <MainSingnupForm>
+                <img src={movieLogo} alt="movie Logo"/>
+                <MainSingnupFormContent>
+                <h1>Sign Up</h1>
 
-            <input type="email" placeholder="Email address"/>
-            <input type="password" placeholder="Password"/>
-            <input type="password" placeholder="Repeat password"/>
+                <input type="email" placeholder="Email address"/>
+                <input type="password" placeholder="Password"/>
+                <input type="password" placeholder="Repeat password"/>
 
-            <button>Create an account</button>
+                <ButtonRed>Create an account</ButtonRed>
 
-            <p> Already have an account? <Link to="/"> Login</Link></p>
-
-        </form>
+                <p> Already have an account? <LinkStyle to="/"> Login</LinkStyle></p>
+                </MainSingnupFormContent>
+            </MainSingnupForm>
+        </MainSingnup>
     )
 }
