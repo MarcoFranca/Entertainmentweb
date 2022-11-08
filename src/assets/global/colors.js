cb7f89bb0bddfaad45850a6471421831f3821634
Marco Franca <dev.marcotullio@gmail.com>
2022-11-07 21:05:31 -0300

    update colors [update]

--- src/assets/global/colors.js
+++ src/assets/global/colors.js
@@ -1,4 +1,9 @@
 export const colors={
     colorAlert: "rgb(248,18,18)",
+    red: "rgba(252,71,71)",
+    darkblue: "rgba(16,20,30)",
+    greyishBlue: "rgba(90,105,143)",
+    semiDarkBlue: "rgba(22,29,47)",
+    pureWhite: "rgba(255,255,255)"
 
 }
