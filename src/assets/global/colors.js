d0d83fa349448b950caa93bdefbb67c7725a327c
Marco Franca <dev.marcotullio@gmail.com>
2022-11-08 14:45:38 -0300

    new colors [UPDATE]

--- src/assets/global/colors.js
+++ src/assets/global/colors.js
@@ -4,6 +4,8 @@ export const colors={
     darkblue: "rgba(16,20,30)",
     greyishBlue: "rgba(90,105,143)",
     semiDarkBlue: "rgba(22,29,47)",
-    pureWhite: "rgba(255,255,255)"
+    pureWhite: "rgba(255,255,255)",
+    filterRed: "invert(66%) sepia(100%) saturate(5969%) hue-rotate(339deg) brightness(116%) contrast(98%)",
+    filterChecked: "brightness(0) saturate(100%) invert(100%) sepia(3%) saturate(635%) hue-rotate(243deg) brightness(121%) contrast(100%)"
 
 }
